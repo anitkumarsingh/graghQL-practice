4fa3119cb4b9992ee2ad4b7be66cf8bd1b8d554a
anitkumarsingh <anikumarsingh99@gmail.com>
2023-02-28 00:43:12 +0530

    resolver

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const typesArray = loadFilesSync(path.join(__dirname,'**/*.graphql'))
+const typesArray = loadFilesSync(path.join(__dirname,'**/*.graphql'));
+
 const schema = makeExecutableSchema({
-  typeDefs:typesArray
+  typeDefs:typesArray,
+  resolvers:{
+    Query:{
+      products:async(parent,current,argv,info)=>{
+        console.log('Products ....',current,argv,info);
+        return await Promise.resolve(parent.products);
+      },
+      orders:(parent,current,argv,info)=>{
+        console.log('Order....');
+        return parent.orders;
+      }
+    }
+  }
 })
 
 const root = {
