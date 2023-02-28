1e2886bdf597a2875e0a696452fd40947fd9b96e
anitkumarsingh <anikumarsingh99@gmail.com>
2023-02-28 22:50:56 +0530

    added apollo-express-server

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
 const express = require('express');
 const path = require('path');
 const { loadFilesSync } = require('@graphql-tools/load-files');
-const { graphqlHTTP } = require('express-graphql');
+const { ApolloServer } = require('apollo-server-express');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 const resolverArray = loadFilesSync(path.join(__dirname, '**/*.resolver.js'));
 
-const schema = makeExecutableSchema({
-	typeDefs: typesArray,
-	resolvers: resolverArray
-});
+async function startApolloServer() {
+	const app = express();
+
+	const schema = makeExecutableSchema({
+		typeDefs: typesArray,
+		resolvers: resolverArray
+	});
+	const server = new ApolloServer({
+		schema
+	});
+	await server.start();
+	server.applyMiddleware({
+		app,
+		path: '/graphql'
+	});
+	app.listen(PORT, () => {
+		console.log(`Server is running! on ${PORT}`);
+	});
+}
 
 // const root = {
 // 	products: require('./products/products.model'),
 // 	orders: require('./orders/order.model')
 // };
 
-app.use(
-	'/graphql',
-	graphqlHTTP({
-		schema,
-		graphiql: true
-	})
-);
-app.listen(PORT, () => {
-	console.log(`Server is running! on ${PORT}`);
-});
+startApolloServer();
